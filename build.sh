f19b29db0d622ff05f18266512da0736ab74621a
kamlu <kamlu@tencent.com>
2024-04-04 18:39:14 +0800

    remove debug marco for production

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-gcc -D CO_DEBUG -o proxy proxy.c socks5.c ip_geo.c -I ./includes -I. -L./lib -lcask -lmaxminddb -lpthread -ldl
+gcc  -o proxy proxy.c socks5.c ip_geo.c http.c picohttpparser.c -I ./includes -I. -L./lib -lcask -lmaxminddb -lpthread -ldl
 
